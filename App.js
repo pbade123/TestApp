31fdda46dcaad8d8dfb255059a00e6998b755acf
Pradip <pradip.b@amazatic.com>
2021-05-11 14:31:32 +0530

    Initial Commit.

---
+++ App.js
@@ -0,0 +1,76 @@
+import React from 'react';
+import {
+  Alert,
+  Dimensions,
+  Pressable,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from 'react-native';
+
+import {Button} from 'react-native-elements';
+
+const App = () => {
+  return (
+    <SafeAreaView style={{flex: 1}}>
+      <StatusBar />
+      <View style={styles.container}>
+        {/* <TouchableOpacity
+          style={styles.button}
+          activeOpacity={0.5}
+          onPress={() => console.log('pressed')}>
+          <View style={{alignItems: 'flex-start', elevation: 4}}>
+            <Text>Left</Text>
+          </View>
+          <View style={{alignItems: 'center', elevation: 4}}>
+            <Text style={styles.buttonText}>Button</Text>
+          </View>
+          <View style={{alignItems: 'flex-end', elevation: 4}}>
+            <Text>Right</Text>
+          </View>
+        </TouchableOpacity> */}
+        <Button
+          icon={{
+            name: 'arrow-right',
+            size: 15,
+            color: 'white',
+          }}
+          title="Button with icon object"
+        />
+        {/* <Button
+          title="Press me"
+          onPress={() => Alert.alert('Simple Button pressed')}
+        /> */}
+      </View>
+    </SafeAreaView>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  button: {
+    borderRadius: 60,
+    padding: 10,
+    width: '80%',
+    height: Dimensions.get('screen').width / 8,
+    elevation: 2,
+    backgroundColor: 'cyan',
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  buttonText: {
+    fontSize: 16,
+    color: 'black',
+  },
+});
+
+export default App;
